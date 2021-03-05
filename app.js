e502636e9da193bb5e5e4e7ec6f8a6de974e75e3
oxdead <som0@meta.ua>
2021-03-05 16:54:18 +0200

    contacts, videos, images, links

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', (e) =>{
+
     document.querySelectorAll(".nav-link").forEach((navlink) => {
         navlink.addEventListener('click', (e) => {
             e.stopPropagation();
@@ -14,6 +15,19 @@ document.addEventListener('DOMContentLoaded', (e) =>{
             document.querySelector(".navbar-toggler").click();
         });
     });
-});
 
+    document.querySelectorAll(".panel-grad").forEach((panelgrad) => {
+        panelgrad.addEventListener('mousemove', (e) => {
+            const docWidth = document.body.clientWidth;
+            const docHeight = document.body.clientHeight;
+    
+            const mouseXpercentage = Math.round(e.pageX / docWidth * 100);
+            const mouseYpercentage = Math.round(e.pageY / docHeight * 100);
+    
+            //const panelGrad = document.querySelector(".panel-grad");
+            e.currentTarget.style.background = 'radial-gradient(at ' + mouseXpercentage + '% ' + mouseYpercentage + '%, #231437, #162520)';
+        });
+    });
+    
+});
 
